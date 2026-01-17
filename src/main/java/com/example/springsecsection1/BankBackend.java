575fa4cd7fdfa1bdb2ebe30d8e167c8b54f3a088
vedaantdutt <vedaantdutt@gmail.com>
2026-01-17 12:20:28 +0530

    welcome controller defined

--- src/main/java/com/example/springsecsection1/BankBackend.java
+++ src/main/java/com/example/springsecsection1/BankBackend.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springsecsection1Application {
+public class BankBackend {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springsecsection1Application.class, args);
+		SpringApplication.run(BankBackend.class, args);
 	}
 
 }
--- src/test/java/com/example/springsecsection1/BankBackendTests.java
+++ src/test/java/com/example/springsecsection1/BankBackendTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Springsecsection1ApplicationTests {
+class BankBackendTests {
 
 	@Test
 	void contextLoads() {
