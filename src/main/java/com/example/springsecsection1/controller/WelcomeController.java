9d46376b7ff769390ae85474b1306340f6cd6700
vedaantdutt <vedaantdutt@gmail.com>
2026-01-17 12:44:36 +0530

    spring security added

--- src/main/java/com/example/springsecsection1/controller/WelcomeController.java
+++ src/main/java/com/example/springsecsection1/controller/WelcomeController.java
@@ -8,6 +8,6 @@ public class WelcomeController {
 
     @GetMapping("/welcome")
     public String welcome(){
-        return "Welcome to Spring Application without security.";
+        return "Welcome to Spring Application with security.";
     }
 }
