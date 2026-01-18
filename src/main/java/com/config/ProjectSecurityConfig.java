cf5bfaee2a6e6eb2e26b9e8437a9c871165b5952
vedaantdutt <52231559+vedaantdutt@users.noreply.github.com>
2026-01-18 12:14:34 +0530

    compromised password checker

--- src/main/java/com/config/ProjectSecurityConfig.java
+++ src/main/java/com/config/ProjectSecurityConfig.java
@@ -4,11 +4,16 @@ import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilte
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -40,12 +45,27 @@ public class ProjectSecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user=User.withUsername("username")
-                .password("12345").authorities("read")
+                .password("{bcrypt}$2a$12$jP8bH79517IpVRpTbrHBo.LK4QKiEBpMTgv0CffmKlUEt0O6M0Zay")
+//                .password("{noop}password")
+                .authorities("read")
                 .build();
+        //dPsS89108!
         UserDetails admin=User.withUsername("admin")
-                .password("54321").authorities("admin")
+                .password("{bcrypt}$2a$12$jP8bH79517IpVRpTbrHBo.LK4QKiEBpMTgv0CffmKlUEt0O6M0Zay")
+                .authorities("admin")
                 .build();
 
-
+        return new InMemoryUserDetailsManager(user,admin);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+
 }
